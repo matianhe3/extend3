b33b83849e7e7f30f93b81fb37e575329aed595d
matianhe3 <matianhe3@gmail.com>
2021-11-02 11:43:15 +0800

    update

--- popup.js
+++ popup.js
@@ -11,6 +11,12 @@ getImg.addEventListener("click", async () => {
     },
     (urls) => {
       var num = urls[0].result;
+      var roomID = tab.url.match(/detail\/(\d+)(\.|$)/);
+      if (roomID && roomID.length > 0) {
+        roomID = roomID[1];
+      } else {
+        roomID = "null";
+      }
       if (num) {
         var numB = document.getElementById("num");
         numB.innerHTML = num.length;
@@ -22,23 +28,29 @@ getImg.addEventListener("click", async () => {
             `" height="150" width="150" style="margin:5px" />`;
         }
         var albumEle = document.getElementById("album");
+        var roomIDEle = document.getElementById("roomID");
         albumEle.innerHTML = album;
-        console.log(albumEle, album);
+        roomIDEle.innerHTML = roomID;
         chrome.storage.sync.set({ urls: num });
       }
     }
   );
 });
 
+chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
+  var path = document.getElementById("roomID").innerHTML;
+  suggest({ filename: path + "/" + item.filename });
+});
+
 download.addEventListener("click", async () => {
   chrome.storage.sync.get("urls", ({ urls }) => {
-    console.log("urls", urls);
     for (const url of urls) {
       chrome.downloads.download({
         url: url,
       });
     }
   });
+  chrome.storage.sync.set({ urls: [] });
 });
 
 function parse() {
@@ -56,8 +68,13 @@ function parse() {
     var text = img.textContent;
     img = imgs.iterateNext();
     var pattern = text.match(re);
-    if (pattern.length > 1) {
+    if (pattern && pattern.length > 1) {
       var url = pattern[1];
+      var pixel = url.match(/thumb.*(_.*_.*)\./);
+      if (pixel && pixel.length > 0) {
+        url = url.replace(pixel[1], "");
+      }
+      url = url.replace("/thumb", "");
     }
     urls.push(url);
   }
